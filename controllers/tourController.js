e777d4e78dc0a2d7d38ce6540f64a67207b026e0
demonicirfan <hashtag.irfan@gmail.com>
2021-07-23 18:36:23 +0530

    working code commit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,6 +16,7 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,4 @@
 const fs = require('fs');
-
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-sample.json`) //?The __dirname in a node script returns the path of the folder where the current JavaScript file resides.
-  );
-  
-
-//!----------------------Functions that work inside GET PUT POST methods in routes -----------------------------
-
  exports.getAllUsers = (req, res) => {
     res.status(500).json({
       status: 'error',
