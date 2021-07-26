d1e1455aadf10f2524836b6f260960c0ca31a795
demonicirfan <hashtag.irfan@gmail.com>
2021-07-26 17:55:00 +0530

    code working

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 const express = require('Express');
-const app = express();
-const tourRouter = require('./routes/tourRouter')
-const userRouter = require('./routes/userRouter')
-
 const morgan = require('morgan');
+const tourRouter = require('./routes/tourRouter');
+const userRouter = require('./routes/userRouter');
+const app = express();
 
-//?---------------------------- 1) MIDDLEWEARS --------------------------------
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+} 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/../public`));
 app.use((req, res, next) => {
-  console.log('hello from the middlewear 👋🏻');
   next();
 });
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
-//*------------------------------ 2) ROUTES MIDDLEWEARS  --------------------
-
-app.use("/api/v1/tours", tourRouter)
-app.use("/api/v1/users", userRouter)
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
